c4961b5ca5a15c238d677588dcdb85974ec23c32
raghavddps2 <raghav.ddps2@gmail.com>
2019-03-03 01:22:23 +0530

    Adding routing files and npm install and nodemon instructions

--- node23.js
+++ node23.js
@@ -28,8 +28,37 @@ var server = http.createServer(function(req,res){
     console.log('Requested URL is '+req.url);
     if(req.url === '/home' || req.url === '/'){
         res.writeHead(200,{'content-type':'text/html'});
-        fs.createReadStream(__dirname+'./index.html').pipe(res);
+        fs.createReadStream(__dirname+'/index.html').pipe(res);
     }
+    //Here, by just providing this if you try something else, it is just gonna rotate same like that.
+    else if(req.url === '/contact'){
+        res.writeHead(200,{'content-type':'text/html'});
+        fs.createReadStream(__dirname+'/contact.html').pipe(res);
+    }
+    else if(req.url  === '/api/ninjas'){
+        var ninjas = [{
+            name: 'Ryu',
+            age: 30,
+            course: 'B.E.'
+        },{
+            name: 'Cran',
+            age: 20,
+            course: 'B.E.'
+        },{
+            name: 'SuperCrazy',
+            age: 10,
+            course: 'B.E.'
+        }]
+        res.writeHead(200,{'content-type':'application/json'});
+        res.end(JSON.stringify(ninjas));
+    }
+    else{
+        res.writeHead(404,{'content-type':'text/html'});
+        fs.createReadStream(__dirname+'/404.html').pipe(res);
+    }
+    
 })
 console.log('Server started at 3000');
 server.listen(3000,'127.0.0.1');
+
+//This is how we create routes.Furthur we are gonna look for routing, which is gonna make this so simpler comparatively.
---
+++ node24.js
@@ -0,0 +1,34 @@
+//So, as we learnt routing, it becomes cumbersome when we have many pages in our applications. So, to reduce the effort we can have
+//a package called as express, whhich makes the same thing very very simpler as comapred tpo what we were thinking about.
+
+/*
+
+    So, to just add a new package what we can do is basically, use something called as the command 
+        npm install express.
+    
+    This will set up express here. We just installed express and it will install other hellpful modules as well.
+
+    So, to remove express basically what we have to do is just foollow the command called as:
+        npm uninstall express.
+
+    Now we are gonna talk about the package.json file. The package.Json file 
+    We can always install any number of packages, but it is always a good idea, to keep track as to which packages areinstalled
+    and on which packages your application depends upon.
+
+    Package.json will keep track of the packagees we install.
+    So, we will try this by installing express again.
+
+    You will have to save the dependency so, we basically use what is called as the:
+        npm install express --save,
+        //Now automatically or magically a dependency will come, which is basicaly express here, in the package.json file.
+        //Now if you do npm unistall express, the dependecy will go.
+        To install all inn dependency on a single click we use npm install.
+
+
+
+        //Next, we have an interesting package called as nodemon, so what nodemon does basically is that whenever we change
+        any file during the development of our application, we actually have no need to chnage anything, 
+        The server is automatically restrated, so this basically reduces the load for restarting the server again and again.
+
+        Now to start this you will not use node (app name), instead you will use nodemon (appname).js;
+*/
